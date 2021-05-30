d33ee5497b58bd319b00f57f2c3f26f9ee67b325
Hector Ruiz <hruiz.com@gmail.com>
2021-05-30 17:42:34 -0500

    deploy

--- api/database/db.js
+++ api/database/db.js
@@ -5,9 +5,19 @@ const { Sequelize } = require('sequelize');
 const {
     DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-    logging: false,
-    native: false,
+const sequelize = new Sequelize({
+    database: DB_NAME,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+    },
 });
 
 //creando modelo de empleados
